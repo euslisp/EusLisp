c0b2d67c3a6a3919102586d2f338dda1d7eff2e4
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2013-05-21 00:28:00 +0000

    update html directory to version 9.00

---
+++ doc/html/jmanual-labels.pl
@@ -0,0 +1,149 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/Contact/;
+$external_labels{$key} = "$URL/" . q|jmanual-node120.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:peg-in-hole/;
+$external_labels{$key} = "$URL/" . q|jmanual-node120.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/threadmodel/;
+$external_labels{$key} = "$URL/" . q|jmanual-node94.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/JointCoords/;
+$external_labels{$key} = "$URL/" . q|jmanual-node152.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/mthread/;
+$external_labels{$key} = "$URL/" . q|jmanual-node91.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/parathreads/;
+$external_labels{$key} = "$URL/" . q|jmanual-node95.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/IPC/;
+$external_labels{$key} = "$URL/" . q|jmanual-node56.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/threadobj/;
+$external_labels{$key} = "$URL/" . q|jmanual-node97.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/synchports/;
+$external_labels{$key} = "$URL/" . q|jmanual-node99.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Coordinates/;
+$external_labels{$key} = "$URL/" . q|jmanual-node110.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/BodyComposition/;
+$external_labels{$key} = "$URL/" . q|jmanual-node118.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/xvariables/;
+$external_labels{$key} = "$URL/" . q|jmanual-node130.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/panellayout/;
+$external_labels{$key} = "$URL/" . q|jmanual-node136.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/xdraw/;
+$external_labels{$key} = "$URL/" . q|jmanual-node131.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:peg-in-a-hole/;
+$external_labels{$key} = "$URL/" . q|jmanual-node120.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/License/;
+$external_labels{$key} = "$URL/" . q|jmanual-node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/textviewpanel/;
+$external_labels{$key} = "$URL/" . q|jmanual-node139.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ManipulatorModel/;
+$external_labels{$key} = "$URL/" . q|jmanual-node150.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/MARS/;
+$external_labels{$key} = "$URL/" . q|jmanual-node153.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/panelitem/;
+$external_labels{$key} = "$URL/" . q|jmanual-node140.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tracking/;
+$external_labels{$key} = "$URL/" . q|jmanual-node148.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Drawings/;
+$external_labels{$key} = "$URL/" . q|jmanual-node127.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:robotst/;
+$external_labels{$key} = "$URL/" . q|jmanual-node155.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Geometry/;
+$external_labels{$key} = "$URL/" . q|jmanual-node112.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/MARSOverview/;
+$external_labels{$key} = "$URL/" . q|jmanual-node155.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/primitive-body-creation/;
+$external_labels{$key} = "$URL/" . q|jmanual-node117.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/animdemo/;
+$external_labels{$key} = "$URL/" . q|jmanual-node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/UnixProcess/;
+$external_labels{$key} = "$URL/" . q|jmanual-node85.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Pathnames/;
+$external_labels{$key} = "$URL/" . q|jmanual-node61.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Pointer/;
+$external_labels{$key} = "$URL/" . q|jmanual-node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ObjectFig/;
+$external_labels{$key} = "$URL/" . q|jmanual-node14.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/compiler/;
+$external_labels{$key} = "$URL/" . q|jmanual-node66.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/PBMfile/;
+$external_labels{$key} = "$URL/" . q|jmanual-node149.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ClassHierarchy/;
+$external_labels{$key} = "$URL/" . q|jmanual-node15.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+1;
+
--- doc/html/table.pl
+++
@@ -1,3 +0,0 @@
-<LI><A NAME="tex2html1" HREF="node8.html#381">Directories in <TT>*eusdir*</TT></A>
-<LI><A NAME="tex2html31" HREF="node21.html#640">EusLisp's special forms</A>
-<LI><A NAME="tex2html707" HREF="node100.html#12923">Parallel gains of programs executed on multi-processors</A>
