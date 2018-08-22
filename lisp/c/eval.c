56bc0ec79a73d7d97737d54a573204fa5746d910
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2018-08-22 09:55:24 +0900

    remove eval.c:892:11: warning: assignment makes pointer from integer without a cast [-Wint-conversion]      ifunc = (((eusinteger_t)ifunc)&0xffffffff00000000)

--- lisp/c/eval.c
+++ lisp/c/eval.c
@@ -889,8 +889,8 @@ pointer args[];
   double f;
 
   if (code->c.fcode.entry2 != NIL) {
-    ifunc = (((eusinteger_t)ifunc)&0xffffffff00000000) 
-      | (intval(code->c.fcode.entry2)&0x00000000ffffffff);
+    ifunc = (eusinteger_t (*)())((((eusinteger_t)ifunc)&0xffffffff00000000)
+                                 | (intval(code->c.fcode.entry2)&0x00000000ffffffff));
     /* R.Hanai 090726 */
   }
   
