8bea663af8c58cfe321c46752b354e8e1a6daec9
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2020-02-05 22:00:33 +0900

    use__SIZEOF_SIZE_T__ to check 64bit machine

--- lisp/c/eus.h
+++ lisp/c/eus.h
@@ -9,7 +9,7 @@
 /****************************************************************/
 
 
-#if (alpha || IRIX6 || x86_64 || aarch64)
+#if (__SIZEOF_SIZE_T__ == 8) || (alpha || IRIX6 || x86_64 || aarch64 || ppc64le)
 #define WORD_SIZE 64
 #else
 #define WORD_SIZE 32
