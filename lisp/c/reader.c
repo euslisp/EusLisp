bb743552b0de85082611bec6f95347b26d02bb68
Kei Okada <kei.okada@gmail.com>
2020-02-11 10:03:23 +0000

    use WORD_SIZE to mask 'unsol & 3L'

--- lisp/c/reader.c
+++ lisp/c/reader.c
@@ -252,10 +252,10 @@ eusinteger_t labx;
 #if sun3 || (!alpha && system5) || sanyo
     unsolp=(pointer *)unsol;
 #endif
-#if sun4 || vax || news || mips || alpha || i386
-    unsolp=(pointer *)((eusinteger_t)unsol & ~3);/*???? */
-#elif (WORD_SIZE == 64)
+#if (WORD_SIZE == 64)
     unsolp=(pointer *)((eusinteger_t)unsol & ~3L);/*???? */
+#else
+    unsolp=(pointer *)((eusinteger_t)unsol & ~3);/*???? */
 #endif
     unsol= *unsolp;
     pointer_update(*unsolp,result); }
