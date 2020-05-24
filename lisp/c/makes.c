7b64430dafe67bbbcb1954616febc9ed42ada52b
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2020-05-25 07:07:03 +0900

    add &0x03 before makeint(entry2), to avoid creating bignum

--- lisp/c/makes.c
+++ lisp/c/makes.c
@@ -292,7 +292,7 @@ pointer (*f)();
   fentaddr= (eusinteger_t)f>>2;
   cd->c.code.entry=makeint(fentaddr);
 #if ARM
-  cd->c.code.entry2=makeint((eusinteger_t)f);
+  cd->c.code.entry2=makeint(((eusinteger_t)f)&0x3);
 #endif
   return(cd);}
 
@@ -513,7 +513,7 @@ pointer (*f)();
   clo->c.clo.subrtype=SUBR_FUNCTION;
   clo->c.clo.entry=makeint((eusinteger_t)f>>2);
 #if ARM
-  clo->c.clo.entry2=makeint((eusinteger_t)f);
+  clo->c.clo.entry2=makeint(((eusinteger_t)f)&0x3);
 #endif
   clo->c.clo.env0=e0;
   clo->c.clo.env1=e1; /*makeint((int)e1>>2);*/
