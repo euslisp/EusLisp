5b29cc1371bb59d7405025849d6bc1689bd63898
Kei Okada <kei.okada@gmail.com>
2022-01-31 23:41:17 +0900

    use (unsigend long) for casting pointer to long

--- lisp/c/memory.c
+++ lisp/c/memory.c
@@ -48,7 +48,9 @@ extern pointer QPARAGC;
 extern pointer K_DISPOSE;
 
 char *maxmemory=(char *)0x0;
-#if (WORD_SIZE == 64)
+#if (WORD_SIZE == 64) && defined(aarch64)
+/* aarch64 does not use minmemory, use _end instead */
+#elif (WORD_SIZE == 64)
 char *minmemory=(char *)0xffffffffffffffff;
 #else
 char *minmemory=(char *)0xffffffff;
@@ -78,7 +80,7 @@ register int k;
   if (k<DEFAULTCHUNKINDEX) k=DEFAULTCHUNKINDEX;
   if (QDEBUG && debug) fprintf(stderr,";; newchunk: k=%d\n",k);
   s=buddysize[k];
-  cp=(struct chunk *)((long)malloc((s+2)*sizeof(pointer)+(sizeof(pointer)-1)) & ~(sizeof(pointer)-1));
+  cp=(struct chunk *)((unsigned long)malloc((s+2)*sizeof(pointer)+(sizeof(pointer)-1)) & ~(sizeof(pointer)-1));
 #if defined(RGC)
   set_heap_range((unsigned int)cp,
       (unsigned int)cp + (s+2)*sizeof(pointer)+(sizeof(pointer)-1));
@@ -173,7 +175,7 @@ register struct buddyfree *buddy;
   bnext=b1->b.nextbcell;
   buddy[k].bp= bnext;	/*remove first element*/
   buddy[k].count--;
-  b2= (bpointer)((long)b1+buddysize[k-1]*sizeof(pointer));
+  b2= (bpointer)((unsigned long)b1+buddysize[k-1]*sizeof(pointer));
   if (k==2) {	/*b1 and b2 are of the same size*/
     b1->b.nextbcell=b2;
     b2->b.nextbcell=buddy[k-1].bp;
@@ -522,8 +524,8 @@ register pointer *oldsp;
 
 int mark_state;
 context *mark_ctx;
-long mark_stack_root;
-long mark_buddy_q;
+unsigned long mark_stack_root;
+unsigned long mark_buddy_q;
 
 void markall()
 { register pointer *p,*spsave;
@@ -555,11 +557,11 @@ void markall()
 #endif
       mark(ctx->threadobj);
       mark_state=4;
-      mark_stack_root=(long)ctx->stack;
+      mark_stack_root=(unsigned long)ctx->stack;
 
       /* mark from thread's stack */
       for (p=ctx->stack; p<ctx->vsp; p++) {
-	mark_state=(long)p;
+	mark_state=(unsigned long)p;
 #if (WORD_SIZE == 64)
 	if ((((eusinteger_t)(*p) & 7L)==0L) && 
 #else
@@ -572,7 +574,7 @@ void markall()
       /* mark free list already acquired in local buddy list */
       for (j=1; j<MAXTHRBUDDY; j++) {
 	q=ctx->thr_buddy[j].bp;
-        mark_buddy_q=(long)q;
+        mark_buddy_q=(unsigned long)q;
 	while (q) { markon(q);
 #ifdef MARK_DEBUG
 		    printf( "markall:%d: markon 0x%ld\n", count, q );
